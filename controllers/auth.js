f58f66719db1b8b055858a2274ce3fe38b70d7fd
jennaly <jenna@wicot.io>
2022-09-08 21:48:46 -0700

    add validation to check for existing account/already registered email address

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,24 +40,39 @@ exports.postSignup = (req, res) => {
     crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', async function(err, password) {
         if (err) { return res.render('error') }
 
-        const user = await User.create(
-            {
-            name: req.body.name,
-            email: req.body.email,
-            hashedPassword: password.toString('hex'),
-            salt: salt
-            }
-        );
-        
-        const userObject = {
-            id: user._id,
-            email: user.email,
-        };
+        User.findOne({$or: [
+        { email: req.body.email },
+        { userName: req.body.userName }
+        ]}, async (err, existingUser) => {
+            if (err) { return next(err) }
+            if (existingUser) {
+                return res.render('signup', {
+                    'existingAccountError': 'PriceKicks account already exists'
+                })
+            } else {
+                const user = await User.create(
+                    {
+                    name: req.body.name,
+                    email: req.body.email,
+                    hashedPassword: password.toString('hex'),
+                    salt: salt
+                    }
+                );
+            
+                const userObject = {
+                    id: user._id,
+                    email: user.email,
+                };
 
-        req.login(userObject, function (err) {
-            if (err) { return res.render('error') };
-            res.redirect('/');
-        });
+                user.save((err) => {
+                    if (err) { return next(err) }
+                    req.login(userObject, function (err) {
+                        if (err) { return res.render('error') };
+                        res.redirect('/');
+                    });
+                })
+            }
+        })
     })
 }
 
